392c8b0dcccf015b6114be39b2bbcd2dd9a2cb50
Mohd Arzaid <arzaid0101030@gmail.com>
2026-09-18 11:53:48 +0530

    Add ShopPage component with FrontendLayout integration

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,9 @@
+import FrontendLayout from "@/components/layouts/frontend-layout";
+
+export default function ShopPage() {
+  return (
+    <FrontendLayout>
+      <div>ShopPage</div>
+    </FrontendLayout>
+  );
+}
