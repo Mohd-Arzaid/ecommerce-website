7df75437a636fb33ba4bef49b417d6d59e487a1a
Mohd Arzaid <arzaid0101030@gmail.com>
2026-09-21 21:09:44 +0530

    Add form validation to SignupPage using Zod and React Hook Form
    - Integrated Zod for schema validation in the signup form.
    - Utilized React Hook Form for form handling and submission.
    - Added error handling for form fields: name, email, and password.
    - Updated package.json and package-lock.json to include @hookform/resolvers and react-hook-form dependencies.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,10 +1,42 @@
+"use client";
+
 import FrontendLayout from "@/components/layouts/frontend-layout";
 import Button from "@/components/ui/button";
 import Input from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
+import { z } from "zod";
+
+const SignUpSchema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters."),
+  email: z.email("Please enter a valid email address."),
+  password: z.string().min(6, "Password must be at least 6 characters long."),
+});
+
+// z.infer gets the TypeScript type from the Zod schema for ex : {name: string, email: string, password: string}
+type SignUpFormValues = z.infer<typeof SignUpSchema>;
 
 const SignupPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormValues>({
+    // Connects React Hook Form with Zod for form validation
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: SignUpFormValues) => {
+    console.log(data);
+  };
+
   return (
     <FrontendLayout>
       <section className="flex min-h-[70vh] items-center justify-center py-16">
@@ -21,20 +53,33 @@ const SignupPage = () => {
           </div>
 
           {/* form */}
-          <form className="space-y-5 mt-8">
-            <Input label="Full Name" placeholder="John Doe" type="text" />
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-5 mt-8">
+            <Input
+              label="Full Name"
+              placeholder="John Doe"
+              type="text"
+              {...register("name")}
+              error={errors.name?.message}
+            />
             <Input
               label="Email Address"
               placeholder="email@gmail.com"
               type="email"
+              {...register("email")}
+              error={errors.email?.message}
             />
             <Input
               label="Password"
               placeholder="Create a password"
               type="password"
+              {...register("password")}
+              error={errors.password?.message}
             />
 
-            <Button fullWidth>Create Account</Button>
+            <Button fullWidth disabled={isSubmitting}>
+              {isSubmitting ? "Creating Account..." : "Create Account"}
+            </Button>
+
             <Button
               fullWidth
               leftIcon={<FcGoogle size={18} />}
